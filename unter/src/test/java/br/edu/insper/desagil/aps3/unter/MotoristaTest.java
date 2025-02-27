f967e2496e259312ebe0be8ac9054992e957d15f
Dnimf <danielmf1@al.insper.edu.br>
2025-02-27 18:16:34 -0300

    motoristaTest

--- unter/src/test/java/br/edu/insper/desagil/aps3/unter/MotoristaTest.java
+++ unter/src/test/java/br/edu/insper/desagil/aps3/unter/MotoristaTest.java
@@ -1,4 +1,24 @@
 package br.edu.insper.desagil.aps3.unter;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MotoristaTest {
+    Motorista motorista;
+    @BeforeEach
+    void setUp(){
+        motorista=new Motorista("1234","a");
+    }
+    @Test
+    void controi(){
+        assertEquals("1234",motorista.getCpf());
+        assertEquals("a",motorista.getNome());
+    }
+    @Test
+    void mudaNome(){
+        motorista.setNome("b");
+        assertEquals("b",motorista.getNome());
+    }
 }
