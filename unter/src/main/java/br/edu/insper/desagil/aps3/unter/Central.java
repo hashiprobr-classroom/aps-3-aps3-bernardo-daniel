b16d91c347a732253c025ba0673e98997adc2f1a
Dnimf <danielmf1@al.insper.edu.br>
2025-02-27 18:51:24 -0300

    central

--- unter/src/main/java/br/edu/insper/desagil/aps3/unter/Central.java
+++ unter/src/main/java/br/edu/insper/desagil/aps3/unter/Central.java
@@ -1,4 +1,37 @@
 package br.edu.insper.desagil.aps3.unter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Central {
+    List<Corrida> corridas = new ArrayList<>();
+    public void adiciona(Corrida corrida){corridas.add(corrida);}
+    public double mediaPassageiro (String cpf){
+        double soma = 0;
+        double contador = 0;
+        for(Corrida corrida : corridas){
+            Passageiro passageiro = corrida.getPassageiro();
+            if(passageiro.getCpf().equals(cpf)){
+                if(corrida.getNotaPassageiro()>0){
+                    soma+=corrida.getNotaPassageiro();
+                    contador +=1;}
+            }
+
+        }
+        return soma/contador;
+    }
+    public double mediaMotorista(String cpf){
+        double soma=0;
+        double contador =0;
+        for(Corrida corrida : corridas){
+            Motorista motorista =corrida.getMotorista();
+            if(motorista.getCpf().equals(cpf)){
+                if(corrida.getNotaMotorista()>0){
+                    soma+=(double) corrida.getNotaMotorista();
+                    contador+=1;
+                }
+            }
+        }
+        return soma/contador;
+    }
 }
