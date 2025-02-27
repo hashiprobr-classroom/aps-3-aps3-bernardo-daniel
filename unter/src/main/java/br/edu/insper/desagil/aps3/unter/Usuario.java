c6919aeefd6b43f980b6235fe56de6defe25982a
Dnimf <danielmf1@al.insper.edu.br>
2025-02-27 15:00:47 -0300

    Usuario

--- unter/src/main/java/br/edu/insper/desagil/aps3/unter/Usuario.java
+++ unter/src/main/java/br/edu/insper/desagil/aps3/unter/Usuario.java
@@ -1,4 +1,23 @@
 package br.edu.insper.desagil.aps3.unter;
 
 public class Usuario {
+    String  cpf;
+    String nome;
+    public Usuario(String cpf, String nome){
+        this.cpf=cpf;
+        this.nome=nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void avalia(Corrida corrida,int nota){}
 }
