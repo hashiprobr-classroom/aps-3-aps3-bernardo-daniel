7d376070d9c390649783d1df389af4993ca8861d
Bernardo Sarcinelli <bernardohns@al.edu.br>
2025-02-27 19:37:24 -0300

    Teste Passageiro

--- unter/src/test/java/br/edu/insper/desagil/aps3/unter/PassageiroTest.java
+++ unter/src/test/java/br/edu/insper/desagil/aps3/unter/PassageiroTest.java
@@ -7,18 +7,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PassageiroTest {
     Passageiro passageiro;
+    Motorista motorista;
+    Corrida corrida;
+
     @BeforeEach
     void setUp(){
         passageiro = new Passageiro("1234","hanna");
+        motorista = new Motorista("4321","Jorge");
+        corrida = new Corrida(passageiro);
     }
+
     @Test
     void constroi(){
         assertEquals("1234",passageiro.getCpf());
         assertEquals("hanna",passageiro.getNome());
     }
+
     @Test
     void mudaNome(){
         passageiro.setNome("a");
         assertEquals("a",passageiro.getNome());
+    }
+
+    @Test
+    void avalia(){
+        passageiro.avalia(corrida,3);
+        assertEquals(3,corrida.getNotaMotorista());
+    }
+
+    @Test
+    void avaliaBaixo(){
+        passageiro.avalia(corrida,0);
+        assertEquals(1,corrida.getNotaMotorista());
+    }
+
+    @Test
+    void avaliaAlto(){
+        passageiro.avalia(corrida,6);
+        assertEquals(5,corrida.getNotaMotorista());
     }
 }
